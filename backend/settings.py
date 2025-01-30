16a4513833b8ebb188d9f4cbc767b8775a853e7e
hosseinim018 <hosseinim018@gmail.com>
2025-01-30 12:43:45 +0330

    change databate to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -105,14 +105,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'database/db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mainvvr_db',
+        'USER': 'postgres',
+        'PASSWORD': 'uWC5nkxDcdJHSmG5f8cz',
+        'HOST': 'main-pdv-service',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
